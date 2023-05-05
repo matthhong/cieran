c0df08ea62fda3ae79d6c65898fbe932186a3aa0
Matt Hong <7455622+matt1hong@users.noreply.github.com>
2023-05-04 23:40:39 -0400

    Dependencies working for Python 3.8

--- setup.py
+++ setup.py
@@ -1,23 +1,34 @@
 from setuptools import setup, find_packages
 
+install_requires = [
+    'coloraide==1.4',
+    'ipywidgets>=8.0.0',
+    'matplotlib>=3.5.0',
+    'networkx>=2.7.0',
+    'numpy>=1.21.0',
+    'scipy>=1.10.0'
+]
+
+dev_requires = [
+    'altair',
+    'jupyter',
+    'vega-datasets'
+]
+
 setup(
-    name='Cieran',
+    name='cieran',
     version='0.0.1',
     description='Designing Colormaps with a Teachable Robot',
     author='Matt-Heun Hong',
     packages=find_packages(),
-    install_requires=[
-        'coloraide==2.2.2',
-        'ipywidgets>=8.0.4',
-        'matplotlib>=3.7.0',
-        'networkx>=2.8.8',
-        'numpy>=1.24.2',
-        'scipy>=1.10.1'
-    ],
+    install_requires=install_requires,
+    extras_require={
+        'dev': dev_requires
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.8',
 )
